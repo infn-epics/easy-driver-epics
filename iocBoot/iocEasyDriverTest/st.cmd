a59f80b2c1b291fe58506c5ed5bad691299c9503
amichelo <andrea.michelotti@lnf.infn.it>
2025-01-21 14:10:07 +0100

    tested

--- iocBoot/iocEasyDriverTest/st.cmd
+++ iocBoot/iocEasyDriverTest/st.cmd
@@ -2,14 +2,14 @@
 
 ###############################################################################
 # Set up environment
-epicsEnvSet("P","$(P=easyDriver:)")
-epicsEnvSet("EASY_DRIVER_91","$(EASY_DRIVER_91=192.168.0.10:10001)")
-< envPaths
-cd "${TOP}"
+epicsEnvSet("P","$(P=BTF:)")
+epicsEnvSet("EASY_DRIVER_91","$(EASY_DRIVER_91=192.168.191.177:10001)")
+#< envPaths
+#cd "${TOP}"
 
 ###############################################################################
 # Register all support components
-dbLoadDatabase "dbd/EasyDriverTest.dbd"
+dbLoadDatabase "../../dbd/EasyDriverTest.dbd"
 EasyDriverTest_registerRecordDeviceDriver pdbbase
 
 ###############################################################################
@@ -20,8 +20,8 @@ asynSetTraceIOMask("L1_TCP",-1,0x2)
 
 ###############################################################################
 # Load record instances
-dbLoadRecords "db/asynRecord.db" "P=$(P),R=asyn,PORT=L1_TCP,ADDR=0,OMAX=0,IMAX=0"
-dbLoadRecords "db/devEasyDriver.db" "P=$(P),R=91:,PORT=L1,RANGE=5,NELM=10000"
+dbLoadRecords "../../db/asynRecord.db" "P=$(P),R=asyn,PORT=L1_TCP,ADDR=0,OMAX=0,IMAX=0"
+dbLoadRecords "../../db/devEasyDriver.db" "P=$(P),R=CHHTB201:,PORT=L1,RANGE=5,NELM=10000"
 
 ###############################################################################
 # Start IOC
